090e30ad2cb96565fcc56515e3f3e7c18a0107a3
maltebp <maltebp94@gmail.com>
2019-10-01 21:11:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Opgave: https://docs.google.com/document/d/1YyJa7KwQISyCBJ7GEIDSohXK_XGcidgemQPpUj8xTHY/edit#heading=h.s08yi2d0eqo0
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
