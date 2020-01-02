403b2c7bddbfcf7c4e11ed925e6f2078fb73e893
maltebp <maltebp94@gmail.com>
2020-01-02 15:49:07 +0100

    Converted back from kotlin file

--- app/src/main/java/com/example/hangman/fragments/game/Game.java
+++ app/src/main/java/com/example/hangman/fragments/game/Game.java
@@ -1,11 +1,8 @@
 package com.example.hangman.fragments.game;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.DisplayMetrics;
